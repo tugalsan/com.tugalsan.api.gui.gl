fcb1cdaa0f1518b2cf48cee6e778e9ab6dacefbd
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:58:27 +0300

    Initial commit

---
+++ src/main/java/moduleinfo.java
@@ -0,0 +1,18 @@
+//module com.tugalsan.api.gui.gl {
+//    requires gwt.user;
+//    requires webgl4j;
+//    requires joml.gwt;
+//    requires three4g;
+//    requires three4g.extensions;
+//    requires datgui4g;
+//    requires jsinterop.annotations;
+//    requires base;
+//    requires elemental2.core;
+//    requires com.tugalsan.api.executable;
+//    requires com.tugalsan.api.shape;
+//    requires com.tugalsan.api.log;
+//    requires com.tugalsan.api.list;
+//    requires com.tugalsan.api.math;
+//    requires com.tugalsan.api.cast;
+//    requires com.tugalsan.api.string;
+//}
